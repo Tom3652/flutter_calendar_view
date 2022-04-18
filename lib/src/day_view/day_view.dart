5ca0c2ab66dc085653e2e222ac4679e8a61d5cd7
Nico3652 <50270977+Nico3652@users.noreply.github.com>
2022-04-18 19:19:43 +0200

    print add

--- lib/src/day_view/day_view.dart
+++ lib/src/day_view/day_view.dart
@@ -313,11 +313,11 @@ class DayViewState<T> extends State<DayView<T>> {
                       physics: BouncingScrollPhysics(),
                       onPageChanged: _onPageChange,
                       itemBuilder: (_, index) {
-                        final date = DateTime(_minDate.year, _minDate.month, _minDate.day + (index-1));
+                        print("total days : $_totalDays");
+                        final date = DateTime(_minDate.year, _minDate.month, _minDate.day + (index));
                         print("date day in page view : ${date.day} + index ----- ${index}");
                         return InternalDayViewPage<T>(
-                          key: ValueKey(
-                              _hourHeight.toString() + date.toString()),
+                          //key: ValueKey(_hourHeight.toString() + date.toString()),
                           width: _width,
                           liveTimeIndicatorSettings: _liveTimeIndicatorSettings,
                           timeLineBuilder: _timeLineBuilder,
