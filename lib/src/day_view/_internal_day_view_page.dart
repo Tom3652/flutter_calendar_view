8fd85e7aa8c4b291f5b5349c0cdcdd24fa5f4938
Nico3652 <50270977+Nico3652@users.noreply.github.com>
2022-04-18 18:41:38 +0200

    print add

--- lib/src/day_view/_internal_day_view_page.dart
+++ lib/src/day_view/_internal_day_view_page.dart
@@ -94,10 +94,9 @@ class InternalDayViewPage<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<CalendarEventData<T>> eventsAllDay = controller
-        .getEventsOnDay(date)
-        .where((element) => element.allDay)
-        .toList();
+    List<CalendarEventData<T>> list = controller.getEventsOnDay(date);
+    print("list 1 : $list");
+    final List<CalendarEventData<T>> eventsAllDay = list.where((element) => element.allDay).toList();
     final isEventAllDay = eventsAllDay.isNotEmpty;
     print("is Event all day for DayView : $isEventAllDay");
     print("all days event : ${eventsAllDay}");
