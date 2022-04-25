4aeb2520ae18682b51b59f1ef1b470359bd84702
Nico3652 <50270977+Nico3652@users.noreply.github.com>
2022-04-25 14:47:26 +0200

    push

--- lib/src/event_controller.dart
+++ lib/src/event_controller.dart
@@ -206,12 +206,11 @@ class EventController<T> extends ChangeNotifier {
       print("is today return true : event in range");
       return true;
     }
-    return false;
     final differenceStart = selectedDate.millisecondsSinceEpoch - start.millisecondsSinceEpoch; //selectedDate.difference(start).inMilliseconds;
     final differenceEnd = end.millisecondsSinceEpoch - selectedDate.millisecondsSinceEpoch;// end.difference(selectedDate).inMilliseconds;
     print("Difference start : $differenceStart");
     print("Difference end : $differenceEnd");
-    if (differenceStart >= 0 && (differenceEnd >= 0 || (differenceEnd < 0 && end.day == selectedDate.day))) {
+    if (differenceStart >= 0 && (differenceEnd >= 0 || (differenceEnd < 0 && isToday(end, selectedDate)))) {
       print("Start is before selected AND end is after selected so we don't remove");
       return true;
     }
